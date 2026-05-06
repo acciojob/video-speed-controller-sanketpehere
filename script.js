959eb6634918d67772c137d28dec72ec76096bf5
Sanket Pehere <105560181+sanketpehere@users.noreply.github.com>
2026-05-06 22:16:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-const inputs = document.querySelectorAll('.controls input');
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const toggle = player.querySelector(".toggle");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const volume = player.querySelector('input[name="volume"]');
+const playbackSpeed = player.querySelector('input[name="playbackRate"]');
+const skipButtons = player.querySelectorAll("[data-skip]");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Play / Pause toggle
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// Update button icon
+function updateButton() {
+  toggle.textContent = video.paused ? "►" : "❚ ❚";
+}
+
+// Update progress bar
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = percent + "%";
+}
+
+// Set progress on click
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+// Volume control
+function changeVolume() {
+  video.volume = this.value;
+}
+
+// Playback speed control
+function changeSpeed() {
+  video.playbackRate = this.value;
+}
+
+// Skip buttons
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Event listeners
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+
+volume.addEventListener("input", changeVolume);
+playbackSpeed.addEventListener("input", changeSpeed);
+
+skipButtons.forEach((btn) => btn.addEventListener("click", skip));
+
+progress.addEventListener("click", scrub);
